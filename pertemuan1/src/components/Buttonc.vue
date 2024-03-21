e1747c1b47b5872382a2402347efe113a6ff2271
marifahmad <marifahmad145@gmail.com>
2024-03-21 15:04:50 +0700

    ekperimen buat button

---
+++ pertemuan1/src/components/Buttonc.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="btn">
+        Submit
+    </button>
+</template>
+
+<!-- css vue 3 -->
+
+<style scoped>
+.btn{
+        background-color: red;
+        color: white;
+        font-size: 28px;
+    }
+</style>
---
+++ pertemuan1/src/components/TextHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Ini Text Header
+    </div>
+</template>
